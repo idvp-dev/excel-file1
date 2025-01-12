693756f87bb539d08c5bbf72178d3eb94dd459f0
idvp-dev <idvegap@gmail.com>
2025-01-12 17:08:03 -0500

    Add files via upload

--- excel.py
+++ excel.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+import pip
+pip.main(["install","openpyxl"])
+
 st.title('Mi primera pagina web con Streamlit')
 
 df=pd.read_excel('VentasTienda.xlsx')
